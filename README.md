c0714bea28d18a48abde08a144efd81a7cfe60c1
Eldio de Almeida Barbosa Neto <97411284+Eldio-Neto@users.noreply.github.com>
2022-02-09 11:16:09 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# DrawPage
+Draw page with HTML, CSS e JS 
+
+Fala galera, mais um projeto usando HTML, CSS e JS.
+
+Dessa vez fiz uma pagina de sorteios utilizando uma lista alimentada pelo usuario.
+
+O Html e CSS estão bem simples, o foco dessa aplicação era testar os meus conhecimentos em JS.
+
+Usando os conceitos que eu aprendi no projeto do To Do List tirei de letra essa aplicação ;)
+
+https://eldio-neto.github.io/DrawPage/
---
+++ SRC/JS/script.js
@@ -0,0 +1,90 @@
+var participantes = []
+
+function idGenerator() {
+    var timeStamp = new Date()
+    var id = timeStamp.getHours().toString() +
+        timeStamp.getMinutes().toString() +
+        timeStamp.getSeconds().toString() +
+        timeStamp.getMilliseconds().toString();
+
+    return id
+
+}
+
+
+function createPerson() {
+    if (document.getElementById("names").value != "") {
+        var participante = document.getElementById("names").value;
+
+        var list = {
+
+            id: idGenerator(),
+            data: {
+                description: participante
+            }
+
+        }
+
+        participantes.push(list)
+        updateScreen()
+    }
+    else{
+        alert("You must to write a name")
+    }
+
+
+}
+
+
+function updateScreen() {
+
+    var list1 = "<ul>"
+
+    participantes.forEach((list => {
+
+        list1 += "<li id-data=" + list.id + ">" + list.data.description + "<button  onclick= removePerson(this) id-data=" + list.id + ">" + "remove</button>" + "</li>"
+
+    }))
+    list1 += "</ul>"
+
+    document.getElementById("participantList").innerHTML = list1;
+    document.getElementById("names").value = ""
+
+}
+
+function removePerson(element) {
+
+    participantes = participantes.filter(list => list.id != element.getAttribute("id-data"))
+
+    updateScreen()
+
+
+}
+
+
+
+function sort() {
+
+    let vencedor = participantes;
+
+    let np = vencedor.length;
+
+    let sp = Math.floor(Math.random() * np)
+
+    document.getElementById("Winner").innerText = "Quem ganhou foi " + vencedor[sp].data.description
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
